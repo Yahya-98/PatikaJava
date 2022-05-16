b24b91dfc6ad47b5108900266f5494668939a938
Yahya DEMİR <80379897+Yahya-98@users.noreply.github.com>
2022-05-16 15:29:17 +0300

    Not ortalamasi pratigini ekledim gecme notunu kullaniciya soruyor

---
+++ java_calismalari/src/NotOrtalamasiHesabi.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class NotOrtalamasiHesabi {
+    public static void main(String[] args) {
+        // Veriables created
+        int mat, fizik, kimya, turkce, tarih, muzik, ort;
+
+        // Created scanner
+        Scanner input = new Scanner((System.in));
+
+        // Data input
+        System.out.print("Matematik Notu Giriniz ");
+        mat = input.nextInt();
+
+
+        System.out.print("Fizik Notu Giriniz ");
+        fizik = input.nextInt();
+
+
+        System.out.print("Kimya Notu Giriniz ");
+        kimya = input.nextInt();
+
+
+        System.out.print("Turkce Notu Giriniz ");
+        turkce = input.nextInt();
+
+
+        System.out.print("Tarih Notu Giriniz ");
+        tarih = input.nextInt();
+
+
+        System.out.print("Muzik Notu Giriniz ");
+        muzik = input.nextInt();
+
+        System.out.print(("Gecme Notu Giriniz "));
+        ort = input.nextInt();
+
+        double result = (mat + fizik + kimya + turkce + tarih + muzik) / 6.0;
+        System.out.println("Ortalamanız :" + result);
+
+        if (result >= ort){
+            System.out.print("Sınıfı Gecti");
+        }
+        else{
+            System.out.print("Sınıfta Kaldı");
+        }
+    }
+}
