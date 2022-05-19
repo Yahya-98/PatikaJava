103d308d4c6494e1daee1f900d0f52f98088524e
Yahya DEMİR <80379897+Yahya-98@users.noreply.github.com>
2022-05-19 14:56:29 +0300

    Zodyak burcu tahmini

---
+++ CinZodyagi.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class CinZodyagi {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int yil;
+
+        System.out.print("Dogum yilinizi giriniz: ");
+        yil = input.nextInt();
+        switch (yil%12){
+            case 0:
+                System.out.println("Çin zodyağı burcunuz Maymun");
+                break;
+            case 1:
+                System.out.println("Çin zodyağı burcunuz Horoz");
+                break;
+            case 2:
+                System.out.println("Çin zodyağı burcunuz Köpek");
+                break;
+            case 3:
+                System.out.println("Çin zodyağı burcunuz Domuz");
+                break;
+            case 4:
+                System.out.println("Çin zodyağı burcunuz Fare");
+                break;
+            case 5:
+                System.out.println("Çin zodyağı burcunuz Öküz");
+                break;
+            case 6 :
+                System.out.println("Çin zodyağı burcunuz Kaplan");
+                break;
+            case 7:
+                System.out.println("Çin zodyağı burcunuz Tavşan");
+                break;
+            case 8:
+                System.out.println("Çin zodyağı burcunuz Ejderha");
+                break;
+            case 9:
+                System.out.println("Çin zodyağı burcunuz Yılan");
+                break;
+            case 10:
+                System.out.println("Çin zodyağı burcunuz  At");
+                break;
+            case 11:
+                System.out.println("Çin zodyağı burcunuz Koyun");
+                break;
+
+        }
+    }
+}
