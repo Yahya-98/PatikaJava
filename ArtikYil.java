1e94805a35804de949eb53c529119ce4a2b298ea
Yahya DEMİR <80379897+Yahya-98@users.noreply.github.com>
2022-05-19 15:10:21 +0300

    Update ArtikYil.java

--- ArtikYil.java
+++ ArtikYil.java
@@ -9,10 +9,15 @@ public class ArtikYil {
         yil = input.nextInt();
 
         if ((yil % 100 == 0) && (yil % 400 == 0)){ // önce 100 e ve 400 e bölümlerden kalanlar kontrol ediliyor
+            
             System.out.println(yil + " bir artık yıldır");
+            
         }else if ((yil % 4 == 0) && (yil % 100 != 0) && (yil% 400 != 0)){ // 100 e ve 400 e bölünmedigi ama 4 e bölündügü kontrol ediliyor
+           
             System.out.println(yil + " bir artık yıldır");
+            
         }else{
+           
             System.out.println(yil + " artık yıl degildir");
         }
 
